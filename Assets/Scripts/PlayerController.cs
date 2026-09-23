43afdf60292198a73912da3b7c3119e083a0ffe6
karysfontana <karysfontana@gmail.com>
2026-09-23 16:28:02 -0400

    Fixed Camera and movement
    The new input system is actually kinda cool

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,19 +8,36 @@ using UnityEngine.Scripting.APIUpdating;
 // [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float speed = 5f; 
     [SerializeField] private float jumpHeight = 5; 
     [SerializeField] private float gravity = -9.8f; 
 
+    [Header("Cam")]
+    // PUT MAIN CAMERA HERE
+    [SerializeField] private Transform cameraTransform; 
+    [SerializeField] private float mouseSensitivity = 15f; 
+    [SerializeField] private float lookLimit = 80f; 
+
     Rigidbody rb;
     Vector3 moveInput; 
+    Vector2 lookInput;
     Vector3 velocity; 
-    CharacterController controller; 
+    CharacterController controller;
+    float verticalRotation = 0f;  
     
     void Start()
     {
         controller = GetComponent<CharacterController>(); 
-        rb = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked; 
+        Cursor.visible = false;
+
+        // In case assignment isn't done in inspector
+        if (cameraTransform == null && Camera.main != null)
+        {
+            cameraTransform = Camera.main.transform;
+        }
     }
 
 
@@ -38,12 +55,31 @@ public class PlayerController : MonoBehaviour
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); 
         }
     }
+
+    public void Look(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
     // Moved this into Update() so it calls more frequently, makes movement smoother
     void Update()
     {
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y); 
+        // Mouse input 
+        float mouseX = lookInput.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = lookInput.y * mouseSensitivity * Time.deltaTime;
+
+        // Rotate player horizontally & camera vertically 
+        transform.Rotate(Vector3.up * mouseX);
+        verticalRotation -= mouseY; 
+        verticalRotation = Mathf.Clamp(verticalRotation, -lookLimit, lookLimit);
+        cameraTransform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f); 
+
+        // Move Player & Cam
+        // Vector3 move = new Vector3(moveInput.x, 0, moveInput.y); 
+        Vector3 move = (transform.forward * moveInput.y) + (transform.right * moveInput.x); 
         controller.Move(move * speed * Time.deltaTime); 
 
+        // Jump
         velocity.y += gravity * Time.deltaTime; 
         controller.Move(velocity * Time.deltaTime); 
 
